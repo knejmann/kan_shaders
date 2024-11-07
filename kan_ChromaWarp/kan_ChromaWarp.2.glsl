82e5b719c1f79a6af4f4a38e395f6fb6616344df
Kåre Nejmann <njm@dr.dk>
2024-11-07 12:24:56 +0100

    bugfix in alternative matte code

--- kan_ChromaWarp/kan_ChromaWarp.2.glsl
+++ kan_ChromaWarp/kan_ChromaWarp.2.glsl
@@ -3,10 +3,10 @@ uniform float adsk_result_w, adsk_result_h;
 uniform float inner_diameter;
 uniform float outer_diameter;
 uniform float width_ratio;
-
+uniform bool alternateMatte;
 float strength;
 
-bool alternateMatte true;
+// bool alternateMatte true;
 // uniform sampler2D chroma_strength;
 
 // void main()
@@ -17,20 +17,20 @@ bool alternateMatte true;
 // }
 
 void main() {
-vec2 uvc = (gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h)) - vec2(0.5);
+    vec2 uvc = (gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h)) - vec2(0.5);
 	// float a = texture2D(chroma_strength, uv).r;
-vec2 aspect = vec2(width_ratio, 1);
-float dist = distance(uvc * aspect, vec2(0.0));
+    vec2 aspect = vec2(width_ratio, 1);
+    float dist = distance(uvc * aspect, vec2(0.0));
 	//strength = clamp((dist - inner_diameter) * outer_diameter, 0.0, 1.0);
-strength = smoothstep(inner_diameter, outer_diameter, dist);
+    strength = smoothstep(inner_diameter, outer_diameter, dist);
 
-if(alternateMatte) {
-vec2 uv = gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h);
-uv *= 1.0 - uv;
-float strength = pow(0.4 * uv.x * uv.y * inner_diameter, outer_diameter);
+    if(alternateMatte) {
+        vec2 uv = gl_FragCoord.xy / vec2(adsk_result_w, adsk_result_h);
+        uv *= 1.0 - uv;
+        strength = pow(0.4 * uv.x * uv.y * inner_diameter, outer_diameter);
 
-}
-gl_FragColor = vec4(strength);
+    }
+    gl_FragColor = vec4(strength);
 }
 
 /* 
@@ -42,7 +42,7 @@ Shadertoys
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
 	vec2 uv = fragCoord.xy / iResolution.xy;
-   
+    
     uv *= ( 1.0 - uv);   //vec2(1.0)- uv.yx; -> 1.-u.yx; Thanks FabriceNeyret !
     
     float vig = uv.x*uv.y * 4.0; // multiply with sth for intensity
