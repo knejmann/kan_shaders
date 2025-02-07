98abe0f33d35e4f78a6899755f26552a5c29a00e
Kåre Nejmann <njm@dr.dk>
2025-02-07 12:56:21 +0100

    Added numeric output of measured luminance. Description updated.

--- kan_ExposureFix/kan_ExposureFix.2.glsl
+++ kan_ExposureFix/kan_ExposureFix.2.glsl
@@ -20,6 +20,65 @@ uniform bool debug;
 uniform vec2 measureCenterUV;
 uniform int measureSize;
 
+// number printing function
+// Smaller Number Printing - @P_Malin
+// Creative Commons CC0 1.0 Universal (CC-0)
+
+// Feel free to modify, distribute or use in commercial code, just don't hold me liable for anything bad that happens!
+// If you use this code and want to give credit, that would be nice but you don't have to.
+
+// I first made this number printing code in https://www.shadertoy.com/view/4sf3RN
+// It started as a silly way of representing digits with rectangles.
+// As people started actually using this in a number of places I thought I would try to condense the 
+// useful function a little so that it can be dropped into other shaders more easily,
+// just snip between the perforations below.
+// Also, the licence on the previous shader was a bit restrictive for utility code.
+//
+// Disclaimer: The values printed may not be accurate!
+// Accuracy improvement for fractional values taken from TimoKinnunen https://www.shadertoy.com/view/lt3GRj
+
+// ---- 8< ---- GLSL Number Printing - @P_Malin ---- 8< ----
+// Creative Commons CC0 1.0 Universal (CC-0) 
+// https://www.shadertoy.com/view/4sBSWW
+
+float DigitBin(const int x) {
+    return x == 0 ? 480599.0 : x == 1 ? 139810.0 : x == 2 ? 476951.0 : x == 3 ? 476999.0 : x == 4 ? 350020.0 : x == 5 ? 464711.0 : x == 6 ? 464727.0 : x == 7 ? 476228.0 : x == 8 ? 481111.0 : x == 9 ? 481095.0 : 0.0;
+}
+
+float PrintValue(vec2 vStringCoords, float fValue, float fMaxDigits, float fDecimalPlaces) {
+    if((vStringCoords.y < 0.0) || (vStringCoords.y >= 1.0))
+        return 0.0;
+
+    bool bNeg = (fValue < 0.0);
+    fValue = abs(fValue);
+
+    float fLog10Value = log2(abs(fValue)) / log2(10.0);
+    float fBiggestIndex = max(floor(fLog10Value), 0.0);
+    float fDigitIndex = fMaxDigits - floor(vStringCoords.x);
+    float fCharBin = 0.0;
+    if(fDigitIndex > (-fDecimalPlaces - 1.01)) {
+        if(fDigitIndex > fBiggestIndex) {
+            if((bNeg) && (fDigitIndex < (fBiggestIndex + 1.5)))
+                fCharBin = 1792.0;
+        } else {
+            if(fDigitIndex == -1.0) {
+                if(fDecimalPlaces > 0.0)
+                    fCharBin = 2.0;
+            } else {
+                float fReducedRangeValue = fValue;
+                if(fDigitIndex < 0.0) {
+                    fReducedRangeValue = fract(fValue);
+                    fDigitIndex += 1.0;
+                }
+                float fDigitValue = (abs(fReducedRangeValue / (pow(10.0, fDigitIndex))));
+                fCharBin = DigitBin(int(floor(mod(fDigitValue, 10.0))));
+            }
+        }
+    }
+    return floor(mod((fCharBin / pow(2.0, floor(fract(vStringCoords.x) * 4.0) + (floor(vStringCoords.y * 5.0) * 4.0))), 2.0));
+}
+// ---- 8< -------- 8< -------- 8< -------- 8< ----
+
 // functions to convert lin to log and back
 
 float convertToLinearFloat(float val) {
@@ -112,6 +171,17 @@ void main(void) {
     // if showMeasurement is enabled the luminance target is plotted on the top 5-6% of the image as a green bar.
     if(showMeasurement && uv.y < 0.95 && uv.y >= 0.94 && uv.x <= (targetLuminance / 1920.))
         colComp = vec3(0.0, 0.5, 0.0);
+
+    // Print the measured luminance value on the screen
+    if(showMeasurement) {
+        vec2 vPixelCoord1 = vec2(100.0, adsk_result_h - 50.0);
+        float fValue1 = lum_disp * 1920.0;
+        vec2 vFontSize = vec2(16.0, 30.0); // Multiples of 4x5 work best
+        float fDigits = 5.0;
+        float fDecimalPlaces = 1.0;
+        float fIsDigit1 = PrintValue((gl_FragCoord - vPixelCoord1) / vFontSize, fValue1, fDigits, fDecimalPlaces);
+        colComp = mix(colComp, vec3(0.0, 1.0, 1.0), fIsDigit1);
+    }
 
     // if enabled the area where the luminance is measured is shown on screen.
     if(showArea) {
